91b685c88edce867dd58c4f7eb2581162d8756af
Heather Cherewaty <hr.cherewaty@gmail.com>
2019-03-12 12:10:03 -0700

    reverseint practice

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,35 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let newNum = '';
+    let str = n.toString();
+    str.split('');
+
+    for(let i = str.length - 1; i >= 0; i--){
+        newNum += str[i];
+
+    }
+
+    if(n < 0) {
+        return parseInt(newNum) * -1;
+    }
+
+   return parseInt(newNum);
+}
+
+
+// function reverseInt(n) {
+//     let newNum = '';
+//     let str = n.toString();
+//     str.split('');
+
+//     for(let i = str.length - 1; i >= 0; i--){
+//         newNum += str[i];
+
+//     }
+
+//    return parseInt(newNum) * Math.sign(n);
+// }
 
 module.exports = reverseInt;

8081f6a95c37c74b6587353eec74d3494b70ac0e
Heather Cherewaty <hr.cherewaty@gmail.com>
2019-03-13 12:54:04 -0700

    practice

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,33 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//     let words = [];
+//     let newStr = str.split(' ');
+
+//     for(let word of newStr){
+//        words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+
+//     return words.join(' ');
+// }
+
+function capitalize(str) {
+    //the first letter will always be capitalized
+    let result = str[0].toUpperCase();
+
+    //starting at index 1 of string:
+    for(let i = 1; i < str.length; i++) {
+        //first check if left of the current character is a space, if it is, capitalize
+        if(str[i - 1] === ' '){
+            //add to result
+            result += str[i].toUpperCase();
+        } else {
+            //otherwise, add element as is to result
+            result += str[i];
+        }
+    }
+    return result;
+}
 
 module.exports = capitalize;
